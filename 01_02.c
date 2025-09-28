edda121e40ab915ca7da47517dcfff2ad0423172
LDK6528 <ldk6528@gmail.com>
2025-09-28 18:43:58 +0900

    1차시 2번문제입니다.

---
+++ 01_02.c
@@ -0,0 +1,15 @@
+//아스키코드(알파벳 소문자, 대문자, 숫자 0-9중 하나가 주어졌을 때, 주어진 글자의 아스키 코드값을 출력하는 프로그램을 작성하시오.)
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void) {
+
+    char input_char;
+
+    scanf("%c", &input_char);
+
+    printf("%d", input_char);
+
+    return 0;
+}
